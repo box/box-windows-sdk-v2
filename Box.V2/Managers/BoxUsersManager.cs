ba7f259dc125cadd7b174842118e48a2c72f7f2b
John Hoerr <jhoerr@iu.edu>
2014-04-22 00:05:41 -0400

    Implement BoxUsersManager.GetEnterpriseUsersAsync

--- Box.V2/Managers/BoxUsersManager.cs
+++ Box.V2/Managers/BoxUsersManager.cs
@@ -1,4 +1,5 @@
-﻿using Box.V2.Auth;
+﻿using System;
+using Box.V2.Auth;
 using Box.V2.Config;
 using Box.V2.Converter;
 using Box.V2.Extensions;
@@ -49,5 +50,29 @@ namespace Box.V2.Managers
             return response.ResponseObject;
         }
 
+
+        /// <summary>
+        /// Get information about users in an enterprise. This method only works for enterprise admins.
+        /// </summary>
+        /// <param name="filterTerm">Filter the results to only users starting with this value in either the name or the login</param>
+        /// <param name="offset">The record at which to start. (default: 0)</param>
+        /// <param name="limit">The number of records to return. (min: 1; default: 100; max: 1000)</param>
+        /// <param name="fields">The fields to populate for each returned user</param>
+        /// <returns>A BoxCollection of BoxUsers matching the provided filter criteria</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when limit outside the range 0&lt;limit&lt;=1000</exception>
+        public async Task<BoxCollection<BoxUser>> GetEnterpriseUsersAsync(string filterTerm = null, uint offset= 0, uint limit = 100, List<string> fields = null)
+        {
+            if (limit == 0 || limit > 1000) throw new ArgumentOutOfRangeException("limit", "limit must be within the range 1 <= limit <= 1000");
+
+            BoxRequest request = new BoxRequest(_config.UserEndpointUri)
+                .Param("filter_term", filterTerm)
+                .Param("offset", offset.ToString())
+                .Param("limit", limit.ToString())
+                .Param(ParamFields, fields);
+
+            IBoxResponse<BoxCollection<BoxUser>> response = await ToResponseAsync<BoxCollection<BoxUser>>(request).ConfigureAwait(false);
+
+            return response.ResponseObject;
+        }
     }
 }
