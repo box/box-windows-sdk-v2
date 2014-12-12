a16c786ba02ff7b0aa3b5219f5ab58c6ba3335c0
ultimoamore <leoonardoo@yahoo.it>
2014-12-12 09:07:01 +0100

    extend BoxFileLock from BoxEntity, not from BoxItem

--- Box.V2/Models/BoxFileLock.cs
+++ Box.V2/Models/BoxFileLock.cs
@@ -9,10 +9,24 @@ namespace Box.V2.Models
     /// <summary>
     /// Box representation of a file
     /// </summary>
-    public class BoxFileLock : BoxItem
+    public class BoxFileLock : BoxEntity
     {
+        public const string FieldCreatedAt = "created_at";
+        public const string FieldCreatedBy = "created_by";
         public const string FieldExpiresAt = "expires_at";
         public const string FieldIsDownloadPrevented = "is_download_prevented";
+
+        /// <summary>
+        /// The time the lock was created
+        /// </summary>
+        [JsonProperty(PropertyName = FieldCreatedAt)]
+        public DateTime? CreatedAt { get; private set; }
+
+        /// <summary>
+        /// The user who created this lock
+        /// </summary>
+        [JsonProperty(PropertyName = FieldCreatedBy)]
+        public BoxUser CreatedBy { get; private set; }
 
         /// <summary>
         /// The expiration date of this lock
