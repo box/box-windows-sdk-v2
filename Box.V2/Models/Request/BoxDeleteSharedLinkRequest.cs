558d8f05abd6e4417c4b60a2dc20609b1b058a98
Jogy <jogy@sirma.bg>
2015-03-27 18:18:30 +0200

    Added methods to delete a file or folders shared link

---
+++ Box.V2/Models/Request/BoxDeleteSharedLinkRequest.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Box.V2.Models
+{
+    /// <summary>
+    /// The sole purpose of this class is to allow passing null as shared_link property in order to delete a shared link
+    /// </summary>
+    public class BoxDeleteSharedLinkRequest : BoxRequestEntity
+    {
+        /// <summary>
+        /// Allow passing of null to remove the shared link
+        /// </summary>
+        [JsonProperty(PropertyName = "shared_link", NullValueHandling = NullValueHandling.Include)]
+        public BoxSharedLinkRequest SharedLink { get; set; }
+  }
+
+}
