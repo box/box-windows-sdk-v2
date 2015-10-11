9c93ff2e126fa18d03d50a6f36126d23a118ed59
Chad Burnette <chadburnette@me.com>
2015-10-11 13:03:14 -0400

    added all fields to JWT assertion

--- Box.V2.JWTAuth/BoxJWTAuth.cs
+++ Box.V2.JWTAuth/BoxJWTAuth.cs
@@ -3,22 +3,42 @@ using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
 using Org.BouncyCastle.Security;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace Box.V2.JWTAuth
 {
     public class BoxJWTAuth
     {
+        const string AUTH_URL = "https://api.box.com/oauth2/token";
         const string JWT_GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer";
 
-        public static string EnterpriseToken(string enterpriseId, string clientId, string clientSecret, string privateKey, string privateKeyPassword)
+        public static string EnterpriseToken(string enterpriseId, string clientId, string clientSecret, string privateKey, string privateKeyPassword, string publicKeyId=null)
         {
+            byte[] randomNumber = new byte[64];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomNumber);
+            }
+
+            Int32 expiresInUnixTimestamp = (Int32)(DateTime.UtcNow.AddSeconds(30).Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
             var payload = new Dictionary<string, object>()
             {
-                { "sub", "mr.x@contoso.com" },
-                { "exp", 1300819380 }
+                { "iss", clientId },
+                { "sub", enterpriseId },
+                { "box_sub_type", "enterprise" },
+                { "aud", AUTH_URL },
+                { "jti", Convert.ToBase64String(randomNumber) },
+                { "exp", expiresInUnixTimestamp }
             };
+
+            var headers = new Dictionary<string, object>();
+            if (publicKeyId != null) {
+                headers.Add("kid", publicKeyId);
+            }
 
             var pwf = new PEMPasswordFinder(privateKeyPassword);
             AsymmetricCipherKeyPair key;
@@ -28,9 +48,9 @@ namespace Box.V2.JWTAuth
             }
             var rsa = DotNetUtilities.ToRSA((RsaPrivateCrtKeyParameters)key.Private);
 
-            string token = JWT.Encode(payload, rsa, JwsAlgorithm.RS256);
+            string assertion = JWT.Encode(payload, rsa, JwsAlgorithm.RS256, extraHeaders: headers);
 
-            return token;
+            return assertion;
         }
     }
 
