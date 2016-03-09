7861d5f9433ee4fb0c6e9e67f5717b3b2c963225
kiko-rodriguez <kiko-rodriguez@users.noreply.github.com>
2016-03-09 17:02:29 +0100

    Update BoxCollaborationUserRequest.cs
    This update allows to add Collaborations by user-id or group-id
    
    https://box-content.readme.io/reference#add-a-collaboration

--- Box.V2/Models/Request/BoxCollaborationUserRequest.cs
+++ Box.V2/Models/Request/BoxCollaborationUserRequest.cs
@@ -7,7 +7,18 @@ namespace Box.V2.Models
     /// </summary>
     public class BoxCollaborationUserRequest : BoxRequestEntity
     {
+        /// <summary>
+        /// The ID of this user or group
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
 
+        /// <summary>
+        /// Type of collaborator, must be either user or group
+        /// </summary>
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+        
         /// <summary>
         /// An email address (does not need to be a Box user)
         /// </summary>
