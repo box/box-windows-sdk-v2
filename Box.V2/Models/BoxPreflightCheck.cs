de25381641f0b05c9573d4db3e87b30c7a66d324
Chad Burnette <chadburnette@me.com>
2016-08-15 17:36:48 -0400

    added support for preflight checks to return upload_url; added optional parameter to upload and upload new version methods to accept custom Uri

---
+++ Box.V2/Models/BoxPreflightCheck.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Box.V2.Models
+{
+    /// <summary>
+    /// Box representation of a preflight check response
+    /// </summary>
+    public class BoxPreflightCheck
+    {
+        public const string FieldUploadUrl = "upload_url";
+        public const string FieldUploadToken = "upload_token";
+
+        /// <summary>
+        /// The upload URL to optionally use when uploading the file (e.g. when upload acceleration is enabled for your Enterprise).
+        /// </summary>
+        [JsonProperty(PropertyName = FieldUploadUrl)]
+        public string UploadUrl { get; private set; }
+
+        /// <summary>
+        /// Convenience method to create Uri instance from UploadUrl string value
+        /// </summary>
+        public Uri UploadUri
+        {
+            get
+            {
+                return new Uri(this.UploadUrl);
+            }
+        }
+
+        /// <summary>
+        /// Currently not used.
+        /// </summary>
+        [JsonProperty(PropertyName = FieldUploadToken)]
+        public string UploadToken { get; private set; }
+    }
+}
