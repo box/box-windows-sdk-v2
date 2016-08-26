e4dde6637a01e7953f0b898c9de7aa7e528b6626
Marek Vysoký <marek.vysoky@hotovo.org>
2016-08-26 11:30:51 +0200

    Preflight Check https://docs.box.com/reference#preflight-check Adding comments to PreflightCheck, changing processing of result, writing unit test PreflightCheck_ValidResponse_ValidStatus

--- Box.V2/Models/BoxPreflightCheck.cs
+++ Box.V2/Models/BoxPreflightCheck.cs
@@ -11,30 +11,6 @@ namespace Box.V2.Models
     /// </summary>
     public class BoxPreflightCheck
     {
-        public const string FieldUploadUrl = "upload_url";
-        public const string FieldUploadToken = "upload_token";
-
-        /// <summary>
-        /// The upload URL to optionally use when uploading the file
-        /// </summary>
-        [JsonProperty(PropertyName = FieldUploadUrl)]
-        public string UploadUrl { get; private set; }
-
-        /// <summary>
-        /// Convenience method to create Uri instance from UploadUrl string value
-        /// </summary>
-        public Uri UploadUri
-        {
-            get
-            {
-                return new Uri(this.UploadUrl);
-            }
-        }
-
-        /// <summary>
-        /// Currently not used.
-        /// </summary>
-        [JsonProperty(PropertyName = FieldUploadToken)]
-        public string UploadToken { get; private set; }
+       
     }
 }
