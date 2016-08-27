060ecb16601a61077874a969dc9753c5527a9b82
Valer Kovac <valer.kovac@gmail.com>
2016-08-27 16:40:55 +0200

    Upload file updated

--- Box.V2.Test/BoxResourceManagerTest.cs
+++ Box.V2.Test/BoxResourceManagerTest.cs
@@ -27,6 +27,7 @@ namespace Box.V2.Test
 
         protected Uri _baseUri = new Uri(Constants.BoxApiUriString);
         protected Uri _FilesUri = new Uri(Constants.FilesEndpointString);
+        protected Uri _FilesUploadUri = new Uri(Constants.FilesUploadEndpointString);
 
         public BoxResourceManagerTest()
         {
@@ -37,6 +38,7 @@ namespace Box.V2.Test
             _config = new Mock<IBoxConfig>();
 
             _config.SetupGet(x => x.FilesEndpointUri).Returns(_FilesUri);
+            _config.SetupGet(x => x.FilesUploadEndpointUri).Returns(_FilesUploadUri);
 
             _authRepository = new AuthRepository(_config.Object, _service, _converter, new OAuthSession("fakeAccessToken", "fakeRefreshToken", 3600, "bearer"));
         }
@@ -62,6 +64,17 @@ namespace Box.V2.Test
                                null,
                                System.Threading.Thread.CurrentThread.CurrentCulture);
             return inst;
+        }
+
+        public static string HexStringFromBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                var hex = b.ToString("x2");
+                sb.Append(hex);
+            }
+            return sb.ToString();
         }
     }
 }
