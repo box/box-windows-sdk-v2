b589922c1cbc700a7d3d0424a3a3ee7577d36141
Valer Kovac <valer.kovac@gmail.com>
2016-09-04 19:42:23 +0200

    Updated GetInformationAsync description and UT

--- Box.V2.Test/BoxResourceManagerTest.cs
+++ Box.V2.Test/BoxResourceManagerTest.cs
@@ -23,6 +23,7 @@ namespace Box.V2.Test
         protected AuthRepository _authRepository;
 
         protected Uri _baseUri = new Uri(Constants.BoxApiUriString);
+        protected Uri _FilesUri = new Uri(Constants.FilesEndpointString);
 
         public BoxResourceManagerTest()
         {
@@ -31,6 +32,8 @@ namespace Box.V2.Test
             _handler = new Mock<IRequestHandler>();
             _service = new BoxService(_handler.Object);
             _config = new Mock<IBoxConfig>();
+
+            _config.SetupGet(x => x.FilesEndpointUri).Returns(_FilesUri);
 
             _authRepository = new AuthRepository(_config.Object, _service, _converter, new OAuthSession("fakeAccessToken", "fakeRefreshToken", 3600, "bearer"));
         }
