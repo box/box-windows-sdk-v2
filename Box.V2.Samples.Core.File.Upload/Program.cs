bd7cf18e274d5a75cfd0a1466d84293a545c1f01
Yang Hu <yhu@box.com>
2016-10-24 16:55:48 -0700

    Core example : Upload a file

---
+++ Box.V2.Samples.Core.File.Upload/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Box.V2.Config;
+using Box.V2.Models;
+using Box.V2.Auth;
+
+namespace Box.V2.Core.Sample
+{
+    public class Program
+    {
+        private static void Main(String[] args)
+        {
+            try
+            {
+                new Program().ExecuteMainAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private async Task ExecuteMainAsync()
+        {
+            var accessToken = "";
+            Console.WriteLine("Access token: ");
+            accessToken = Console.ReadLine();
+
+            Console.WriteLine("Remote file name: ");
+            var fileName = "";
+            fileName = Console.ReadLine();
+
+            Console.WriteLine("Local file path: ");
+            var localFilePath = "";
+            localFilePath = Console.ReadLine();
+
+            Console.WriteLine("Parent folder Id: ");
+            var parentFolderId = "";
+            parentFolderId = Console.ReadLine();
+
+            var auth = new OAuthSession(accessToken, "YOUR_REFRESH_TOKEN", 3600, "bearer");
+
+            var config = new BoxConfig("YOUR_CLIENT_ID", "YOUR_CLIENT_ID", new Uri("http://boxsdk"));
+            var client = new BoxClient(config, auth);
+
+            var file = File.OpenRead(localFilePath);
+            var fileRequest = new BoxFileRequest
+            {
+                Name = fileName,
+                Parent = new BoxFolderRequest { Id = parentFolderId }
+            };
+
+            var bFile = await client.FilesManager.UploadAsync(fileRequest, file);
+
+            Console.WriteLine(localFilePath + " uploaded to folder: " + parentFolderId + " as file: " + bFile.Id);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Box.V2.Samples.Core.File.Upload/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Box.V2.Samples.Core.File.Upload")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b6646429-8cd9-412e-937d-bd5862b80cb4")]
