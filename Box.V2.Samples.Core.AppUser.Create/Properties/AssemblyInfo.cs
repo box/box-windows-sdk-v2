c6c988f46f932448d8c9b4fc3d51d3bde0d7e237
Yang Hu <yhu@box.com>
2016-10-25 13:59:38 -0700

    JWTAuth into SDK & Demo

---
+++ Box.V2.Samples.Core.AppUser.Create/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Box.V2.Samples.Core.AppUser.Create")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb890c3a-311c-4140-b24c-863b9c8595a7")]
