08a4ded44146fa3deef9726468a7ab293845eda7
Chad Burnette <chadburnette@me.com>
2016-11-08 12:18:53 -0800

    added integration test for shared link

--- Box.V2/Managers/BoxSharedItemsManager.cs
+++ Box.V2/Managers/BoxSharedItemsManager.cs
@@ -35,7 +35,7 @@ namespace Box.V2.Managers
         /// Shared items are any files or folders that are represented by a shared link. Shared items are different from other API resources in that a shared resource doesn’t necessarily have to be in the account of the user accessing it. The actual shared link itself is used along with a normal access token.
         /// </summary>
         /// <param name="sharedLink">The shared link for this item.</param>
-        /// <param name="sharedLinkPassword"></param>
+        /// <param name="sharedLinkPassword">The password for the shared link (if required)</param>
         /// <returns>A full file or folder object is returned if the shared link is valid and the user has access to it. An error may be returned if the link is invalid, if a password is required, or if the user does not have access to the file.</returns>
         public async Task<BoxItem> SharedItemsAsync(string sharedLink, string sharedLinkPassword=null)
         {
