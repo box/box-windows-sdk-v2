3a07f10cc2ca88a3c2a2653186440acca748a5e9
Chad Burnette <chadburnette@me.com>
2016-11-10 11:00:18 -0800

    more work on metadata

--- Box.V2.Test.Integration/BoxMetadataManagerTestIntegration.cs
+++ Box.V2.Test.Integration/BoxMetadataManagerTestIntegration.cs
@@ -41,12 +41,12 @@ namespace Box.V2.Test.Integration
             var update = new BoxMetadataUpdate() { Op = MetadataUpdateOp.copy, Path = "/attr1", From = "/attr4" };
             var update2 = new BoxMetadataUpdate() { Op = MetadataUpdateOp.replace, Path = "/attr4", Value = "value2" };
             var updatedMD = await _client.MetadataManager.UpdateFileMetadataAsync(FILE_ID, new List<BoxMetadataUpdate>() { update, update2 }, SCOPE, TEMPLATE_KEY);
-            Assert.AreEqual(updatedMD["attr1"], ATTR4, "Failed to update metadata on file");
-            Assert.AreEqual(updatedMD["attr4"], "value2", "Failed to update metadata on file");
+            Assert.AreEqual(ATTR4, updatedMD["attr1"], "Failed to update metadata on file");
+            Assert.AreEqual("value2", updatedMD["attr4"], "Failed to update metadata on file");
 
             //get all file metadata
             var allMD = await _client.MetadataManager.GetAllFileMetadataTemplatesAsync(FILE_ID);
-            Assert.AreEqual(allMD.Entries.Count, 1, "Failed to get all file metadata");
+            Assert.AreEqual(1, allMD.Entries.Count, "Failed to get all file metadata");
 
             //delete metadata
             var result = await _client.MetadataManager.DeleteFileMetadataAsync(FILE_ID, SCOPE, TEMPLATE_KEY);
@@ -76,12 +76,12 @@ namespace Box.V2.Test.Integration
             var update = new BoxMetadataUpdate() { Op = MetadataUpdateOp.copy, Path = "/attr1", From = "/attr4" };
             var update2 = new BoxMetadataUpdate() { Op = MetadataUpdateOp.replace, Path = "/attr4", Value = "value2" };
             var updatedMD = await _client.MetadataManager.UpdateFolderMetadataAsync(FOLDER_ID, new List<BoxMetadataUpdate>() { update, update2 }, SCOPE, TEMPLATE_KEY);
-            Assert.AreEqual(updatedMD["attr1"], ATTR4, "Failed to update metadata on folder");
-            Assert.AreEqual(updatedMD["attr4"], "value2", "Failed to update metadata on folder");
+            Assert.AreEqual(ATTR4, updatedMD["attr1"], "Failed to update metadata on folder");
+            Assert.AreEqual("value2", updatedMD["attr4"], "Failed to update metadata on folder");
 
             //get all folder metadata
             var allMD = await _client.MetadataManager.GetAllFolderMetadataTemplatesAsync(FOLDER_ID);
-            Assert.AreEqual(allMD.Entries.Count, 1, "Failed to get all folder metadata");
+            Assert.AreEqual(1, allMD.Entries.Count, "Failed to get all folder metadata");
 
             //delete metadata
             var result = await _client.MetadataManager.DeleteFolderMetadataAsync(FOLDER_ID, SCOPE, TEMPLATE_KEY);
@@ -92,16 +92,16 @@ namespace Box.V2.Test.Integration
         public async Task Metadata_GetAllEnterprise_LiveSession()
         {
             var templates = await _client.MetadataManager.GetEnterpriseMetadataAsync();
-            Assert.AreEqual(templates.Entries.Count, 1, "Failed to get enterprise metadata templates");
-            Assert.AreEqual(templates.Entries.First().TemplateKey, TEMPLATE_KEY, "Failed to get enterprise metadata templates");
+            Assert.IsTrue(templates.Entries.Count >1 , "Failed to get enterprise metadata templates");
+            Assert.IsNotNull(templates.Entries.Single(t=> t.TemplateKey == TEMPLATE_KEY ), "Failed to get enterprise metadata templates");
         }
 
         [TestMethod]
         public async Task Metadata_GetTemplate_LiveSession()
         {
             var template = await _client.MetadataManager.GetMetadataTemplate(SCOPE, TEMPLATE_KEY);
-            Assert.AreEqual(template.TemplateKey, TEMPLATE_KEY, "Failed to get metadata template");
-            Assert.AreEqual(template.Fields.Count, 4, "Failed to get metadata template");
+            Assert.AreEqual(TEMPLATE_KEY, template.TemplateKey, "Failed to get metadata template");
+            Assert.AreEqual(4, template.Fields.Count, "Failed to get metadata template");
         }
 
         [TestMethod]
@@ -115,15 +115,15 @@ namespace Box.V2.Test.Integration
             var options = new List<BoxMetadataTemplateFieldOption>() { new BoxMetadataTemplateFieldOption() { Key = "value1" }, new BoxMetadataTemplateFieldOption() { Key = "value2" } };
             var field4 = new BoxMetadataTemplateField() { Key = "attr4", DisplayName = "a enum", Type = "enum", Options= options };
             var fields = new List<BoxMetadataTemplateField>() { field1, field2, field3, field4 };
-            var templateToCreate = new BoxMetadataTemplate() { TemplateKey = templateKey, DisplayName=templateKey, Fields=fields, Scope=SCOPE };
+            var templateToCreate = new BoxMetadataTemplate() { TemplateKey = templateKey, DisplayName=templateKey, Fields=fields, Hidden=true, Scope=SCOPE };
             var createdTemplate = await _client.MetadataManager.CreateMetadataTemplate(templateToCreate);
-            Assert.AreEqual(createdTemplate.TemplateKey, templateKey, "Failed to create metadata template");
-            Assert.AreEqual(createdTemplate.Fields.Count, 4, "Failed to create metadata template");
-            Assert.AreEqual(createdTemplate.Fields.First(f => f.Key == "attr1").Type, "string");
-            Assert.AreEqual(createdTemplate.Fields.First(f => f.Key == "attr2").Type, "float");
-            Assert.AreEqual(createdTemplate.Fields.First(f => f.Key == "attr3").Type, "date");
-            Assert.AreEqual(createdTemplate.Fields.First(f => f.Key == "attr4").Type, "enum");
-            Assert.AreEqual(createdTemplate.Fields.First(f => f.Key == "attr4").Options.Count, 2);
+            Assert.AreEqual(templateKey, createdTemplate.TemplateKey, "Failed to create metadata template");
+            Assert.AreEqual(4, createdTemplate.Fields.Count, "Failed to create metadata template");
+            Assert.AreEqual("string", createdTemplate.Fields.First(f => f.Key == "attr1").Type, "Failed to create metadata template");
+            Assert.AreEqual("float", createdTemplate.Fields.First(f => f.Key == "attr2").Type, "Failed to create metadata template");
+            Assert.AreEqual("date", createdTemplate.Fields.First(f => f.Key == "attr3").Type, "Failed to create metadata template");
+            Assert.AreEqual("enum", createdTemplate.Fields.First(f => f.Key == "attr4").Type, "Failed to create metadata template");
+            Assert.AreEqual(2, createdTemplate.Fields.First(f => f.Key == "attr4").Options.Count, "Failed to create metadata template");
 
         }
     }
--- Box.V2/Models/BoxMetadataTemplate.cs
+++ Box.V2/Models/BoxMetadataTemplate.cs
@@ -14,18 +14,37 @@ namespace Box.V2.Models
         public const string FieldScope = "scope";
         public const string FieldDisplayName = "displayName";
         public const string FieldFields = "fields";
+        public const string FieldHidden = "hidden";
 
+        /// <summary>
+        /// A unique identifier for the template. The identifier must be unique across the scope of the enterprise to which the metadata template is being applied to. Defaults to a string derived from the displayName if no value is provided.
+        /// </summary>
         [JsonProperty(PropertyName = FieldTemplateKey)]
         public string TemplateKey { get; set; }
 
+        /// <summary>
+        /// The scope of the object. Only the enterprise scope is currently supported.
+        /// </summary>
         [JsonProperty(PropertyName = FieldScope)]
         public string Scope { get; set; }
 
+        /// <summary>
+        /// The display name of the template.
+        /// </summary>
         [JsonProperty(PropertyName = FieldDisplayName)]
         public string DisplayName { get; set; }
 
+        /// <summary>
+        /// The ordered set of key:value pairs for the template.
+        /// </summary>
         [JsonProperty(PropertyName = FieldFields)]
         public List<BoxMetadataTemplateField> Fields { get; set; }
+
+        /// <summary>
+        /// Whether this template is hidden in the UI. Defaults to false.
+        /// </summary>
+        [JsonProperty(PropertyName = FieldHidden)]
+        public bool? Hidden { get; set; }
     }
 
     /// <summary>
@@ -38,12 +57,21 @@ namespace Box.V2.Models
         public const string FieldDisplayName = "displayName";
         public const string FieldOptions = "options";
 
+        /// <summary>
+        /// The data type of the field's value. Currently, there are four attributes supported by templates: string, enum, float, and date (RFC 3339).
+        /// </summary>
         [JsonProperty(PropertyName = FieldType)]
         public string Type { get; set; }
 
+        /// <summary>
+        /// A unique identifier for the field.
+        /// </summary>
         [JsonProperty(PropertyName = FieldKey)]
         public string Key { get; set; }
 
+        /// <summary>
+        /// The display name of the field.
+        /// </summary>
         [JsonProperty(PropertyName = FieldDisplayName)]
         public string DisplayName { get; set; }
 
@@ -61,6 +89,9 @@ namespace Box.V2.Models
     {
         public const string FieldKey = "key";
 
+        /// <summary>
+        /// A unique possible value for the options type
+        /// </summary>
         [JsonProperty(PropertyName = FieldKey)]
         public string Key { get; set; }
     }
