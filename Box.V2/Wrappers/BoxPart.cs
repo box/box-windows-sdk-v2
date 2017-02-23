9ee43b372aec2ce89bb5d2a9da3428d2c89c0075
Yang Hu <yhu@box.com>
2017-02-23 19:17:32 +0000

    UploadPart

---
+++ Box.V2/Wrappers/BoxPart.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Box.V2
+{
+    /// <summary>
+    /// A Box representation of the file part.
+    /// </summary>
+    public class BoxFilePart : IBoxPart<Stream>
+    {
+        public Stream Value { get; set; }
+    }
+}
---
+++ Box.V2/Wrappers/Contracts/IBoxPart.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Box.V2
+{
+    /// <summary>
+    /// Interface that defines a Box part
+    /// </summary>
+    public interface IBoxPart
+    {}
+
+    /// <summary>
+    /// Interface that takes different types of box part
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IBoxPart<T> : IBoxPart
+    {
+        /// <summary>
+        /// The value of the part
+        /// </summary>
+        T Value { get; }
+    }
+}
