795a24ce477397e998543bd2ea1542d9189abfd2
Pal Ramanathan <rpalaniappan@gmail.com>
2017-03-01 21:58:18 +0000

    Status function in SDK and minor refactoring

--- Box.V2/Models/BoxSessionParts.cs
+++ Box.V2/Models/BoxSessionParts.cs
@@ -3,6 +3,9 @@ using Newtonsoft.Json;
 
 namespace Box.V2.Models
 {
+    /// <summary>
+    /// Represents the Parts uploaded in a Session.
+    /// </summary>
     public class BoxSessionParts
     {
         public BoxSessionParts(List<BoxSessionPartInfo> parts)
@@ -11,28 +14,31 @@ namespace Box.V2.Models
         }
 
         /// <summary>
-        /// List of session parts uploaded
+        /// List of session parts uploaded.
         /// </summary>
         [JsonProperty(PropertyName = "parts")]
         public List<BoxSessionPartInfo> Parts { get; set; }
     }
 
+    /// <summary>
+    /// Represents a single part of a session.
+    /// </summary>
     public class BoxSessionPartInfo
     {
         /// <summary>
-        /// String representing the 8 digit part ID
+        /// String representing the 8 digit part ID.
         /// </summary>
         [JsonProperty(PropertyName = "part_id")]
         public string PartId { get; set; }
 
         /// <summary>
-        /// File part offset
+        /// File part offset in bytes.
         /// </summary>
         [JsonProperty(PropertyName = "offset")]
         public long Offset { get; set; }
 
         /// <summary>
-        /// File part Size
+        /// File part Size in bytes.
         /// </summary>
         [JsonProperty(PropertyName = "size")]
         public long Size { get; set; }
