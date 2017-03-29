1d2178a8408d33aadb6fa47e437520e56aa464e8
Yang Hu <yhu@box.com>
2017-03-29 12:55:55 -0700

    rsa-fix from kendomen

--- Box.V2.Core/DotNetUtilities.cs
+++ Box.V2.Core/DotNetUtilities.cs
@@ -8,35 +8,61 @@ namespace Box.V2.JWTAuth
 {
     public static class DotNetUtilities
     {
-        public static RSA ToRSA(RsaPrivateCrtKeyParameters rsaKey)
+        // -------------------------------------------------------------------------------------
+        // When running JWT in an Azure WebJob or Function, you will get an error: 
+        //    "System.Security.Cryptography.CryptographicException: The system cannot find the file specified."
+        //
+        // The error message is misleading and you can see more info here:    
+        //     https://blogs.msdn.microsoft.com/winsdk/2009/11/16/opps-system-security-cryptography-cryptographicexception-the-system-cannot-find-the-file-specified/
+        //
+        // I took the guts of the sealed class DotNetUtilities and overrode it here
+        // -------------------------------------------------------------------------------------
+        public static RSA ToRSA(RsaPrivateCrtKeyParameters privKey)
         {
-            var parameters = new RSAParameters
-                             {
-                                 Modulus = rsaKey.Modulus.ToByteArrayUnsigned(),
-                                 Exponent = rsaKey.PublicExponent.ToByteArrayUnsigned(),
-                                 P = rsaKey.P.ToByteArrayUnsigned(),
-                                 Q = rsaKey.Q.ToByteArrayUnsigned()
-                             };
-            parameters.D = ConvertRsaParametersField(rsaKey.Exponent, parameters.Modulus.Length);
-            parameters.DP = ConvertRsaParametersField(rsaKey.DP, parameters.P.Length);
-            parameters.DQ = ConvertRsaParametersField(rsaKey.DQ, parameters.Q.Length);
-            parameters.InverseQ = ConvertRsaParametersField(rsaKey.QInv, parameters.Q.Length);
-
-            var cryptoServiceProvider = RSA.Create();
-            cryptoServiceProvider.ImportParameters(parameters);
-            return cryptoServiceProvider;
+            return CreateRSAProvider(ToRSAParameters(privKey));
         }
 
-        private static Byte[] ConvertRsaParametersField(BigInteger n, Int32 size)
+        private static RSA CreateRSAProvider(RSAParameters rp)
         {
-            var byteArrayUnsigned = n.ToByteArrayUnsigned();
-            if (byteArrayUnsigned.Length == size)
-                return byteArrayUnsigned;
-            if (byteArrayUnsigned.Length > size)
-                throw new ArgumentException("Specified size too small", nameof(size));
-            var numArray = new Byte[size];
-            Array.Copy(byteArrayUnsigned, 0, numArray, size - byteArrayUnsigned.Length, byteArrayUnsigned.Length);
-            return numArray;
+            CspParameters csp = new CspParameters();
+            csp.KeyContainerName = string.Format("BouncyCastle-{0}", Guid.NewGuid());
+
+            // This needs to be here to run as WebJob
+            csp.Flags = CspProviderFlags.UseMachineKeyStore;
+
+            RSACryptoServiceProvider rsaCsp = new RSACryptoServiceProvider(csp);
+            rsaCsp.ImportParameters(rp);
+
+            return rsaCsp;
+        }
+
+        // ------------------------------------------------------------------------------------------------------------------
+        // Note: http://stackoverflow.com/questions/28370414/import-rsa-key-from-bouncycastle-sometimes-throws-bad-data
+        // ------------------------------------------------------------------------------------------------------------------
+        private static RSAParameters ToRSAParameters(RsaPrivateCrtKeyParameters privKey)
+        {
+            RSAParameters rp = new RSAParameters();
+            rp.Modulus = privKey.Modulus.ToByteArrayUnsigned();
+            rp.Exponent = privKey.PublicExponent.ToByteArrayUnsigned();
+            rp.P = privKey.P.ToByteArrayUnsigned();
+            rp.Q = privKey.Q.ToByteArrayUnsigned();
+            rp.D = ConvertRSAParametersField(privKey.Exponent, rp.Modulus.Length);
+            rp.DP = ConvertRSAParametersField(privKey.DP, rp.P.Length);
+            rp.DQ = ConvertRSAParametersField(privKey.DQ, rp.Q.Length);
+            rp.InverseQ = ConvertRSAParametersField(privKey.QInv, rp.Q.Length);
+            return rp;
+        }
+
+        private static byte[] ConvertRSAParametersField(BigInteger n, int size)
+        {
+            byte[] bs = n.ToByteArrayUnsigned();
+            if (bs.Length == size)
+                return bs;
+            if (bs.Length > size)
+                throw new ArgumentException("Specified size too small", "size");
+            byte[] padded = new byte[size];
+            Array.Copy(bs, 0, padded, size - bs.Length, bs.Length);
+            return padded;
         }
 
         public static string GetSha1Hash(Stream stream)
