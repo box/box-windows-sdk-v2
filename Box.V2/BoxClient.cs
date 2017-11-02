9a85297d4e42fdd76da41ebba3e6d3196414cc39
carycheng77 <c4cheng@ucsd.edu>
2017-11-02 23:11:39 +0000

    added fix to config

--- Box.V2/BoxClient.cs
+++ Box.V2/BoxClient.cs
@@ -132,7 +132,7 @@ namespace Box.V2
             DevicePinManager = new BoxDevicePinManager(Config, _service, _converter, Auth, _asUser, _suppressNotifications);
             WebLinksManager = new BoxWebLinksManager(Config, _service, _converter, Auth, _asUser, _suppressNotifications);
             LegalHoldPoliciesManager = new BoxLegalHoldPoliciesManager(Config, _service, _converter, Auth, _asUser, _suppressNotifications);
-            TermsOfServiceManager = new BoxTermsOfServicesManager(Config, _service, _converter, Auth, _asUser, _suppressNotifications);
+            TermsOfServiceManager = new BoxTermsOfServiceManager(Config, _service, _converter, Auth, _asUser, _suppressNotifications);
 
             // Init Resource Plugins Manager
             ResourcePlugins = new BoxResourcePlugins();
@@ -258,6 +258,6 @@ namespace Box.V2
         /// <summary>
         /// The manager that represents the terms of service endpoint
         /// </summary>
-        public BoxTermsOfServicesManager TermsOfServiceManager { get; private set; }
+        public BoxTermsOfServiceManager TermsOfServiceManager { get; private set; }
     }
 }
