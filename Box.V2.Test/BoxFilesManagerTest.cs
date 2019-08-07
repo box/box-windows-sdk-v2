d59838ab78f4969663732713ba500ac154dad8b2
Matt Willer <mwiller@box.com>
2019-08-07 12:52:08 -0700

    Add missing fields to file and folder models (#582)

--- Box.V2.Test/BoxFilesManagerTest.cs
+++ Box.V2.Test/BoxFilesManagerTest.cs
@@ -9,6 +9,7 @@ using System.Net.Http.Headers;
 using System.IO;
 using Newtonsoft.Json;
 using System.Text;
+using System.Globalization;
 
 namespace Box.V2.Test
 {
@@ -81,7 +82,85 @@ namespace Box.V2.Test
         public async Task GetFileInformation_ValidResponse_ValidFile()
         {
             /*** Arrange ***/
-            string responseString = "{ \"type\": \"file\", \"id\": \"5000948880\", \"sequence_id\": \"3\", \"etag\": \"3\", \"sha1\": \"134b65991ed521fcfe4724b7d814ab8ded5185dc\", \"name\": \"tigers.jpeg\", \"description\": \"a picture of tigers\", \"size\": 629644, \"path_collection\": { \"total_count\": 2, \"entries\": [ { \"type\": \"folder\", \"id\": \"0\", \"sequence_id\": null, \"etag\": null, \"name\": \"All Files\" }, { \"type\": \"folder\", \"id\": \"11446498\", \"sequence_id\": \"1\", \"etag\": \"1\", \"name\": \"Pictures\" } ] }, \"created_at\": \"2012-12-12T10:55:30-08:00\", \"modified_at\": \"2012-12-12T11:04:26-08:00\", \"trashed_at\": null, \"purged_at\": null, \"content_created_at\": \"2013-02-04T16:57:52-08:00\", \"content_modified_at\": \"2013-02-04T16:57:52-08:00\", \"created_by\": { \"type\": \"user\", \"id\": \"17738362\", \"name\": \"sean rose\", \"login\": \"sean@box.com\" }, \"modified_by\": { \"type\": \"user\", \"id\": \"17738362\", \"name\": \"sean rose\", \"login\": \"sean@box.com\" }, \"owned_by\": { \"type\": \"user\", \"id\": \"17738362\", \"name\": \"sean rose\", \"login\": \"sean@box.com\" }, \"shared_link\": { \"url\": \"https://www.box.com/s/rh935iit6ewrmw0unyul\", \"download_url\": \"https://www.box.com/shared/static/rh935iit6ewrmw0unyul.jpeg\", \"vanity_url\": null, \"is_password_enabled\": false, \"unshared_at\": null, \"download_count\": 0, \"preview_count\": 0, \"access\": \"open\", \"permissions\": { \"can_download\": true, \"can_preview\": true } }, \"parent\": { \"type\": \"folder\", \"id\": \"11446498\", \"sequence_id\": \"1\", \"etag\": \"1\", \"name\": \"Pictures\" }, \"item_status\": \"active\", \"tags\": [ \"important\", \"needs review\" ] }";
+            string responseString = @"{
+                ""type"": ""file"",
+                ""id"": ""5000948880"",
+                ""sequence_id"": ""3"",
+                ""etag"": ""3"",
+                ""sha1"": ""134b65991ed521fcfe4724b7d814ab8ded5185dc"",
+                ""name"": ""tigers.jpeg"",
+                ""description"": ""a picture of tigers"",
+                ""size"": 629644,
+                ""path_collection"": {
+                    ""total_count"": 2,
+                    ""entries"": [
+                        {
+                            ""type"": ""folder"",
+                            ""id"": ""0"",
+                            ""sequence_id"": null,
+                            ""etag"": null,
+                            ""name"": ""All Files""
+                        },
+                        {
+                            ""type"": ""folder"",
+                            ""id"": ""11446498"",
+                            ""sequence_id"": ""1"",
+                            ""etag"": ""1"",
+                            ""name"": ""Pictures""
+                        }
+                    ]
+                },
+                ""created_at"": ""2012-12-12T10:55:30-08:00"",
+                ""modified_at"": ""2012-12-12T11:04:26-08:00"",
+                ""trashed_at"": null,
+                ""purged_at"": null,
+                ""content_created_at"": ""2013-02-04T16:57:52-08:00"",
+                ""content_modified_at"": ""2013-02-04T16:57:52-08:00"",
+                ""created_by"": {
+                    ""type"": ""user"",
+                    ""id"": ""17738362"",
+                    ""name"": ""sean rose"",
+                    ""login"": ""sean@box.com""
+                },
+                ""modified_by"": {
+                    ""type"": ""user"",
+                    ""id"": ""17738362"",
+                    ""name"": ""sean rose"",
+                    ""login"": ""sean@box.com""
+                },
+                ""owned_by"": {
+                    ""type"": ""user"",
+                    ""id"": ""17738362"",
+                    ""name"": ""sean rose"",
+                    ""login"": ""sean@box.com""
+                },
+                ""shared_link"": {
+                    ""url"": ""https://www.box.com/s/rh935iit6ewrmw0unyul"",
+                    ""download_url"": ""https://www.box.com/shared/static/rh935iit6ewrmw0unyul.jpeg"",
+                    ""vanity_url"": null,
+                    ""is_password_enabled"": false,
+                    ""unshared_at"": null,
+                    ""download_count"": 0,
+                    ""preview_count"": 0,
+                    ""access"": ""open"",
+                    ""permissions"": {
+                        ""can_download"": true,
+                        ""can_preview"": true
+                    }
+                },
+                ""parent"": {
+                    ""type"": ""folder"",
+                    ""id"": ""11446498"",
+                    ""sequence_id"": ""1"",
+                    ""etag"": ""1"",
+                    ""name"": ""Pictures"" },
+                ""item_status"": ""active"",
+                ""tags"": [ ""important"", ""needs review"" ],
+                ""expires_at"": ""2020-11-03T22:00:00Z"",
+                ""allowed_invitee_roles"": [ ""editor"" ],
+                ""has_collaborations"": false,
+                ""is_externally_owned"": false
+            }";
             IBoxRequest boxRequest = null;
             Handler.Setup(h => h.ExecuteAsync<BoxFile>(It.IsAny<IBoxRequest>()))
                 .Returns(Task.FromResult<IBoxResponse<BoxFile>>(new BoxResponse<BoxFile>()
@@ -107,6 +186,10 @@ namespace Box.V2.Test
             Assert.AreEqual("https://www.box.com/s/rh935iit6ewrmw0unyul", f.SharedLink.Url);
             Assert.AreEqual("important", f.Tags[0]);
             Assert.AreEqual("needs review", f.Tags[1]);
+            Assert.AreEqual("2020-11-03T22:00:00Z", f.ExpiresAt.Value.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ssZ", DateTimeFormatInfo.InvariantInfo));
+            Assert.AreEqual("editor", f.AllowedInviteeRoles.First());
+            Assert.IsFalse(f.HasCollaborations.Value);
+            Assert.IsFalse(f.IsExternallyOwned.Value);
         }
 
         [TestMethod]
