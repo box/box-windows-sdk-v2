1d67d445bbda3dfa66cd4908fbb3b17398d29228
sujaygarlanka <sujay.garlanka@gmail.com>
2021-01-28 11:19:58 -0500

    clean up files

--- Box.V2.Test/BoxRetentionPoliciesManagerTest.cs
+++ Box.V2.Test/BoxRetentionPoliciesManagerTest.cs
@@ -1,13 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Box.V2.Managers;
 using Box.V2.Models;
-using Box.V2.Config;
-using Box.V2.Auth;
+using Box.V2.Models.Request;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using Box.V2.Models.Request;
-using System;
 
 namespace Box.V2.Test
 {
