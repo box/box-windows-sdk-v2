2c12841b982bc076e209ad265ec7b6d18fab9ae3
mwoda <mwoda@box.com>
2021-09-30 12:36:52 +0200

    Add missing internal

--- Box.V2/Exceptions/BoxAuthenticationFailedException.cs
+++ Box.V2/Exceptions/BoxAuthenticationFailedException.cs
@@ -17,7 +17,7 @@ namespace Box.V2.Exceptions
         /// <param name="error"></param>
         /// <param name="statusCode"></param>
         /// <param name="responseHeaders"></param>
-        protected BoxSessionInvalidatedException(string message, BoxError error, HttpStatusCode statusCode, HttpResponseHeaders responseHeaders)
+        protected internal BoxSessionInvalidatedException(string message, BoxError error, HttpStatusCode statusCode, HttpResponseHeaders responseHeaders)
             : base(message, error, statusCode, responseHeaders)
         {
         }
