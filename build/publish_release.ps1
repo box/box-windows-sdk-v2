c35b935ea6032979da86860c6109d3271768feb0
Mateusz Woda <mwoda@box.com>
2021-12-14 03:27:32 -0800

    chore: release script changes (#783)

--- build/publish_release.ps1
+++ build/publish_release.ps1
@@ -23,11 +23,7 @@ if($NextVersion -eq $null -Or $NextVersion -eq ''){
         $NextVersion = (Select-String -Pattern [0-9]+\.[0-9]+\.[0-9]+ -Path $CHANGELOG_PATH | Select-Object -First 1).Matches.Value
     }
 }
-
-$FRAMEWORK_NUPKG_PATH=$FRAMEWORK_PROJ_DIR + "\" + $FRAMEWORK_ASSEMBLY_NAME + "." + $NextVersion + ".nupkg"
-$CORE_NUPKG_PATH=$CORE_PROJ_DIR + "\bin\Release\" + $CORE_ASSEMBLY_NAME + "." + $NextVersion + ".nupkg"
-$FRAMEWORK_PACKAGE_LINK = "https://www.nuget.org/packages/" + $FRAMEWORK_ASSEMBLY_NAME + "/" + $NextVersion
-$CORE_PACKAGE_LINK = "https://www.nuget.org/packages/" + $CORE_ASSEMBLY_NAME + "/" + $NextVersion
+$NextVersionTag = "v" + $NextVersion
 
 ###########################################################################
 # Parameters validation
@@ -50,49 +46,30 @@ if ($InstallDependencies){
 }
 
 ###########################################################################
-# Extract release notes
-###########################################################################
-
-$VersionFound = $false
-foreach($line in Get-Content $CHANGELOG_PATH) {
-    if($line -match "## [[0-9]+\.[0-9]+\.[0-9]+]"){
-        if($VersionFound){
-            break
-        }
-        $VersionFound = $true
-        continue
-    }
-    if($VersionFound){
-        $ReleaseNotes += "$line`n"
-    }
-}
-$ReleaseNotes = $ReleaseNotes.replace('### ' , '').trim()
-$ReleaseNotes += "`n`n$FRAMEWORK_PACKAGE_LINK`n"
-$ReleaseNotes += "$CORE_PACKAGE_LINK"
-
-###########################################################################
-# Create git release
+# Publish git release
 ###########################################################################
 
 if($DryRun){
-    Write-Output "Dry run. Release will not be created."
+    Write-Output "Dry run. Release will not be update."
 }else{
     $password = ConvertTo-SecureString "$GithubToken" -AsPlainText -Force
     $Cred = New-Object System.Management.Automation.PSCredential ("Release_Bot", $password)
     Set-GitHubAuthentication -SessionOnly -Credential $Cred
+
+    $releases = Get-GitHubRelease -OwnerName $REPO_OWNER -RepositoryName $REPO_NAME
+    $release = ($releases | Where-Object { $_.Name -eq $NextVersionTag })
+    if($release -eq $null -Or $release -eq ''){
+        Write-Output "Release with the name " + $NextVersionTag " not found. Aborting script"
+        exit 1
+    }
+
     $releaseParams = @{
+        Release = $release."ID"
         OwnerName = $REPO_OWNER
         RepositoryName = $REPO_NAME
-        Tag = "v" + $NextVersion
-        Name = "v" + $NextVersion
-        Body = $ReleaseNotes
+        Draft = $false
     }
-    $release = New-GitHubRelease @releaseParams
- 
-    $release | New-GitHubReleaseAsset -Path $FRAMEWORK_NUPKG_PATH
-    $release | New-GitHubReleaseAsset -Path $FRAMEWORK_PDB_PATH
-    $release | New-GitHubReleaseAsset -Path $CORE_NUPKG_PATH
-    $release | New-GitHubReleaseAsset -Path $CORE_PDB_PATH
+    Set-GitHubRelease @releaseParams
 
     Clear-GitHubAuthentication
 }
