9ca67537e9324e51677374170fc45c7fef1d1298
mwoda <mwoda@box.com>
2022-02-04 14:38:59 +0100

    feat: deprecate index_name in ExecuteMetadataQuery

---
+++ Box.V2.Test.IntegrationNew/Configuration/Commands/DisposableCommands/CreateMetadataTemplateCommand.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Box.V2.Models;
+
+namespace Box.V2.Test.IntegrationNew.Configuration.Commands.DisposableCommands
+{
+    public class CreateMetadataTemplateCommand : CommandBase, IDisposableCommand
+    {
+        private readonly List<BoxMetadataTemplateField> _metadataFields;
+
+        public string TemplateKey;
+
+        public CreateMetadataTemplateCommand(string templateKey, List<BoxMetadataTemplateField> metadataFields,
+            CommandScope scope = CommandScope.Test, CommandAccessLevel accessLevel = CommandAccessLevel.Admin) : base(scope, accessLevel)
+        {
+            TemplateKey = templateKey;
+            _metadataFields = metadataFields;
+        }
+
+        public async Task<string> Execute(IBoxClient client)
+        {
+            var metadataTemplate = new BoxMetadataTemplate
+            {
+                DisplayName = TemplateKey,
+                TemplateKey = TemplateKey,
+                Scope = "enterprise",
+                Fields = _metadataFields
+            };
+
+            var response = await client.MetadataManager.CreateMetadataTemplate(metadataTemplate);
+
+            return response.Id;
+        }
+
+        public async Task Dispose(IBoxClient client)
+        {
+            await client.MetadataManager.DeleteMetadataTemplate("enterprise", TemplateKey);
+        }
+    }
+}
