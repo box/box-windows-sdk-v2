38266733e4e328b55dd973cc894a7e5a3086ed9a
mwoda <mwoda@box.com>
2022-04-05 20:41:18 +0200

    simplify unit tests

--- Box.V2.Test/BoxConfigTest.cs
+++ Box.V2.Test/BoxConfigTest.cs
@@ -17,7 +17,7 @@ namespace Box.V2.Test
                 'clientSecret': 'cre-123',
                 'appAuth': {
                   'publicKeyID': 'kid-123',
-                  'privateKey': 'DUMMY',
+                  'privateKey': 'testKey',
                   'passphrase': 'password'
                 },
               },
@@ -27,9 +27,9 @@ namespace Box.V2.Test
             var config = BoxConfigBuilder
                 .CreateFromJsonString(JsonString)
                 .Build();
-            Assert.AreEqual(config.BoxApiUri, new System.Uri(Constants.BoxApiUriString));
+            Assert.AreEqual(config.BoxApiUri, new Uri(Constants.BoxApiUriString));
 
-            var exampleUri = new System.Uri("https://example.com/");
+            var exampleUri = new Uri("https://example.com/");
             config = BoxConfigBuilder.CreateFromJsonString(JsonString)
                 .SetBoxApiUri(exampleUri)
                 .Build();
@@ -46,7 +46,7 @@ namespace Box.V2.Test
                 'clientSecret': 'cre-123',
                 'appAuth': {
                   'publicKeyID': 'kid-123',
-                  'privateKey': 'DUMMY',
+                  'privateKey': 'testKey',
                   'passphrase': 'password'
                 },
               },
@@ -59,19 +59,19 @@ namespace Box.V2.Test
             Assert.AreEqual(config.ClientId, "cid-123");
             Assert.AreEqual(config.ClientSecret, "cre-123");
             Assert.AreEqual(config.JWTPublicKeyId, "kid-123");
-            Assert.AreEqual(config.JWTPrivateKey, "DUMMY");
+            Assert.AreEqual(config.JWTPrivateKey, "testKey");
             Assert.AreEqual(config.EnterpriseId, "eid-123");
         }
 
         [TestMethod]
         public void BoxConfig_SetAuthTokenUriString()
         {
-            var boxConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var boxConfig = new BoxConfigBuilder("", "")
                 .Build();
-            Assert.AreEqual(boxConfig.BoxAuthTokenApiUri, new System.Uri(Constants.BoxAuthTokenApiUriString));
+            Assert.AreEqual(boxConfig.BoxAuthTokenApiUri, new Uri(Constants.BoxAuthTokenApiUriString));
 
             var exampleUri = new System.Uri("https://example.com/token");
-            var newConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var newConfig = new BoxConfigBuilder("", "")
                 .SetBoxTokenApiUri(exampleUri)
                 .Build();
             Assert.AreEqual(newConfig.BoxAuthTokenApiUri, exampleUri);
@@ -80,12 +80,12 @@ namespace Box.V2.Test
         [TestMethod]
         public void BoxConfig_SetAuthRevokeUriString()
         {
-            var boxConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var boxConfig = new BoxConfigBuilder("", "")
                 .Build();
-            Assert.AreEqual(boxConfig.BoxAuthRevokeApiUri, new System.Uri(Constants.BoxAuthRevokeApiUriString));
+            Assert.AreEqual(boxConfig.BoxAuthRevokeApiUri, new Uri(Constants.BoxAuthRevokeApiUriString));
 
-            var exampleUri = new System.Uri("https://example.com/revoke");
-            var newConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var exampleUri = new Uri("https://example.com/revoke");
+            var newConfig = new BoxConfigBuilder("", "")
                 .SetBoxRevokeApiUri(exampleUri)
                 .Build();
             Assert.AreEqual(newConfig.BoxAuthRevokeApiUri, exampleUri);
@@ -94,12 +94,12 @@ namespace Box.V2.Test
         [TestMethod]
         public void BoxConfig_SetAuthAuthorizeUriString()
         {
-            var boxConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var boxConfig = new BoxConfigBuilder("", "")
                 .Build();
             Assert.AreEqual(boxConfig.BoxAuthAuthorizeApiUri, new System.Uri(Constants.BoxAuthAuthorizeApiUriString));
 
-            var exampleUri = new System.Uri("https://example.com/authorize");
-            var newConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var exampleUri = new Uri("https://example.com/authorize");
+            var newConfig = new BoxConfigBuilder("", "")
                 .SetBoxAuthorizeApiUri(exampleUri)
                 .Build();
             Assert.AreEqual(newConfig.BoxAuthAuthorizeApiUri, exampleUri);
@@ -108,12 +108,8 @@ namespace Box.V2.Test
         [TestMethod]
         public void BoxConfig_SetBoxApiHostUri()
         {
-            var boxConfig = new BoxConfigBuilder("", "", "", "", "", "")
-                .Build();
-            Assert.AreEqual(boxConfig.BoxAuthAuthorizeApiUri, new System.Uri(Constants.BoxAuthAuthorizeApiUriString));
-
-            var exampleUri = new System.Uri("https://example.com/base");
-            var newConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var exampleUri = new Uri("https://example.com/base");
+            var newConfig = new BoxConfigBuilder("", "")
                 .SetBoxApiHostUri(exampleUri)
                 .Build();
             Assert.AreEqual(newConfig.BoxAuthTokenApiUri, new Uri(exampleUri, "/oauth2/token"));
@@ -123,12 +119,8 @@ namespace Box.V2.Test
         [TestMethod]
         public void BoxConfig_SetBoxAccountApiHostUri()
         {
-            var boxConfig = new BoxConfigBuilder("", "", "", "", "", "")
-                .Build();
-            Assert.AreEqual(boxConfig.BoxAuthAuthorizeApiUri, new System.Uri(Constants.BoxAuthAuthorizeApiUriString));
-
-            var exampleUri = new System.Uri("https://example.com/account");
-            var newConfig = new BoxConfigBuilder("", "", "", "", "", "")
+            var exampleUri = new Uri("https://example.com/account");
+            var newConfig = new BoxConfigBuilder("", "")
                 .SetBoxAccountApiHostUri(exampleUri)
                 .Build();
             Assert.AreEqual(newConfig.BoxAuthAuthorizeApiUri, new Uri(exampleUri, "/oauth2/authorize"));
