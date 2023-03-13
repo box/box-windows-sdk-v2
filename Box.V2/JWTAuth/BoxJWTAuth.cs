89c88f75fd8fa1aee30f384257e6c945d940202d
mwoda <mwoda@box.com>
2023-03-13 17:58:01 +0100

    fix newline

--- Box.V2/JWTAuth/BoxJWTAuth.cs
+++ Box.V2/JWTAuth/BoxJWTAuth.cs
@@ -265,7 +265,7 @@ namespace Box.V2.JWTAuth
                 expireTime = nowOverride.Value.AddSeconds(30);
             }
 
-            var payload = new JwtPayload(_boxConfig.ClientId, _boxConfig.JWTAudience, 
+            var payload = new JwtPayload(_boxConfig.ClientId, _boxConfig.JWTAudience,
                 claims, null, expireTime.LocalDateTime);
 
             var header = new JwtHeader(signingCredentials: _credentials);
