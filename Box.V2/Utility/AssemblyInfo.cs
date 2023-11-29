3015ce5fbfbee6243f39d29a182f56ea551c128c
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2023-11-29 16:14:02 +0100

    chore: release v5.6.1 (#931)

--- Box.V2/Utility/AssemblyInfo.cs
+++ Box.V2/Utility/AssemblyInfo.cs
@@ -5,7 +5,7 @@ namespace Box.V2.Utility
         public const string AssemblyTitle = "Box.V2";
         public const string AssemblyProduct = "Box.V2";
 
-        public const string NuGetVersion = "5.6.0";
+        public const string NuGetVersion = "5.6.1";
         public const string AssemblyVersion = "1.0.0.0";
         public const string AssemblyFileVersion = "1.0.0.0";
     }
