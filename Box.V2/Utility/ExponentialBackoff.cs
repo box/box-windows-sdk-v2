826f84430ee66afe057ef9e9f8b410f37b6c4f92
Christopher Watford <christopherw@suzy.com>
2024-04-10 08:45:59 -0400

    fix: do not recreate Random each time
    On .NET Framework it is incorrect to recreate Random instances each time, these should instead be one per instance. On .NET Core and later this is not a limitation, but the libraries share a codebase.
    
    Fixes #944

--- Box.V2/Utility/ExponentialBackoff.cs
+++ Box.V2/Utility/ExponentialBackoff.cs
@@ -4,15 +4,16 @@ namespace Box.V2.Utility
 {
     public class ExponentialBackoff : IRetryStrategy
     {
+        private readonly Random _random = new Random();
+
         public TimeSpan GetRetryTimeout(int numRetries)
         {
             var baseInterval = TimeSpan.FromSeconds(2.0);
             const double RETRY_RANDOMIZATION_FACTOR = 0.5;
             var minRandomization = 1 - RETRY_RANDOMIZATION_FACTOR;
             var maxRandomization = 1 + RETRY_RANDOMIZATION_FACTOR;
-            var random = new Random();
 
-            var randomization = random.NextDouble() * (maxRandomization - minRandomization) + minRandomization;
+            var randomization = _random.NextDouble() * (maxRandomization - minRandomization) + minRandomization;
             var exponential = Math.Pow(2, numRetries - 1);
             var result = Math.Ceiling(exponential * baseInterval.TotalSeconds * randomization);
             return TimeSpan.FromSeconds(result);
