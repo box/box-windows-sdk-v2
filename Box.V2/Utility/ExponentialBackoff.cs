d03b1ce65d4077e2895acfce3bc286ea501669aa
Christopher Watford <83599748+watfordsuzy@users.noreply.github.com>
2024-04-15 08:55:57 -0400

    fix: do not recreate Random each time (#945)
    * fix: do not recreate Random each time
    
    On .NET Framework it is incorrect to recreate Random instances each time, these should instead be one per instance. On .NET Core and later this is not a limitation, but the libraries share a codebase.
    
    Fixes #944
    
    * Use a thread safe Random instance #944
    
    * Forgot to commit csproj change #944
    
    * Address PR comment #944

--- Box.V2/Utility/ExponentialBackoff.cs
+++ Box.V2/Utility/ExponentialBackoff.cs
@@ -10,9 +10,10 @@ namespace Box.V2.Utility
             const double RETRY_RANDOMIZATION_FACTOR = 0.5;
             var minRandomization = 1 - RETRY_RANDOMIZATION_FACTOR;
             var maxRandomization = 1 + RETRY_RANDOMIZATION_FACTOR;
-            var random = new Random();
 
-            var randomization = random.NextDouble() * (maxRandomization - minRandomization) + minRandomization;
+            var randomization = ThreadSafeRandom.Instance.NextDouble()
+                * (maxRandomization - minRandomization) + minRandomization;
+
             var exponential = Math.Pow(2, numRetries - 1);
             var result = Math.Ceiling(exponential * baseInterval.TotalSeconds * randomization);
             return TimeSpan.FromSeconds(result);
