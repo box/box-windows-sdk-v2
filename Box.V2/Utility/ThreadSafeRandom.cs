0e66608bed36eeb32d6619704ba5366a4cb8e8b5
Christopher Watford <christopherw@suzy.com>
2024-04-15 08:41:41 -0400

    Address PR comment #944

--- Box.V2/Utility/ThreadSafeRandom.cs
+++ Box.V2/Utility/ThreadSafeRandom.cs
@@ -5,20 +5,15 @@ namespace Box.V2.Utility
 {
     /// <summary>
     /// A thread safe implementation of <see cref="Random"/>, following best practices
-    /// for .NET Framework, .NET Standard, and .NET 6+.
+    /// for .NET Framework and .NET Standard.
     /// </summary>
     /// <seealso href="https://learn.microsoft.com/en-us/dotnet/fundamentals/runtime-libraries/system-random"/>
     internal static class ThreadSafeRandom
     {
-#if NET6_0_OR_GREATER
-        /// <summary>
-        /// An instance of <see cref="Random"/> specific to the calling thread.
-        /// Do not pass this instance to other threads or contexts.
-        /// </summary>
-        public static Random Instance => Random.Shared;
-#else
         //
         // Adapted from https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Random.cs
+        //
+        // NOTE: when/if this library updates to .NET 6+, this code can be replaced with `Random.Shared`
         //
 
         [ThreadStatic]
@@ -32,6 +27,5 @@ namespace Box.V2.Utility
         /// Do not pass this instance to other threads or contexts.
         /// </summary>
         public static Random Instance => _random ?? CreateRandom();
-#endif
     }
 }
