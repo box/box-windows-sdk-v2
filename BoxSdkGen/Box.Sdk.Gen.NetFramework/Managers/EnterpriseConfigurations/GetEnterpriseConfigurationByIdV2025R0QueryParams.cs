5f531d66907a4b35a8d03ff4f8e3847a1eb9af9f
box-sdk-build <sdk-build@box.com>
2025-10-29 07:15:16 -0700

    fix: remove enum types from GET enterprise configurations endpoint (box/box-openapi#560)

--- BoxSdkGen/Box.Sdk.Gen.NetFramework/Managers/EnterpriseConfigurations/GetEnterpriseConfigurationByIdV2025R0QueryParams.cs
+++ BoxSdkGen/Box.Sdk.Gen.NetFramework/Managers/EnterpriseConfigurations/GetEnterpriseConfigurationByIdV2025R0QueryParams.cs
@@ -1,6 +1,7 @@
-using Box.Sdk.Gen;
-using System.Collections.Generic;
+using System;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using Box.Sdk.Gen;
 using Box.Sdk.Gen.Internal;
 using Box.Sdk.Gen.Schemas;
 using Box.Sdk.Gen.Parameters;
@@ -8,12 +9,12 @@ using Box.Sdk.Gen.Parameters;
 namespace Box.Sdk.Gen.Managers {
     public class GetEnterpriseConfigurationByIdV2025R0QueryParams {
         /// <summary>
-        /// The comma-delimited list of the enterprise configuration categories. 
+        /// A comma-separated list of the enterprise configuration categories.
         /// Allowed values: `security`, `content_and_sharing`, `user_settings`, `shield`.
         /// </summary>
-        public string Categories { get; set; }
+        public IReadOnlyList<string> Categories { get; set; }
 
-        public GetEnterpriseConfigurationByIdV2025R0QueryParams(string categories) {
+        public GetEnterpriseConfigurationByIdV2025R0QueryParams(IReadOnlyList<string> categories) {
             Categories = categories;
         }
     }
