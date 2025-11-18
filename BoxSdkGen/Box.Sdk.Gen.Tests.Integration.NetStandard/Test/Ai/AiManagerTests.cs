f57bf233b7c43acfbbf35302185cb65d3a5c196a
box-sdk-build <sdk-build@box.com>
2025-11-18 05:50:24 -0800

    test: Fix AI tests (box/box-codegen#886)

--- BoxSdkGen/Box.Sdk.Gen.Tests.Integration.NetStandard/Test/Ai/AiManagerTests.cs
+++ BoxSdkGen/Box.Sdk.Gen.Tests.Integration.NetStandard/Test/Ai/AiManagerTests.cs
@@ -19,8 +19,8 @@ namespace Box.Sdk.Gen.Tests.Integration {
         public async System.Threading.Tasks.Task TestAskAiSingleItem() {
             AiAgent aiAgentConfig = await client.Ai.GetAiAgentDefaultConfigAsync(queryParams: new GetAiAgentDefaultConfigQueryParams(mode: GetAiAgentDefaultConfigQueryParamsModeField.Ask) { Language = "en-US" });
             FileFull fileToAsk = await new CommonsManager().UploadNewFileAsync();
-            AiResponseFull response = await client.Ai.CreateAiAskAsync(requestBody: new AiAsk(mode: AiAskModeField.SingleItemQa, prompt: "which direction sun rises", items: Array.AsReadOnly(new [] {new AiItemAsk(id: fileToAsk.Id, type: AiItemAskTypeField.File) { Content = "Sun rises in the East" }})));
-            Assert.IsTrue(NullableUtils.Unwrap(response).Answer.Contains("East"));
+            AiResponseFull response = await client.Ai.CreateAiAskAsync(requestBody: new AiAsk(mode: AiAskModeField.SingleItemQa, prompt: "Which direction does the Sun rise?", items: Array.AsReadOnly(new [] {new AiItemAsk(id: fileToAsk.Id, type: AiItemAskTypeField.File) { Content = "The Sun rises in the east" }})));
+            Assert.IsTrue(NullableUtils.Unwrap(response).Answer.Contains("east"));
             Assert.IsTrue(NullableUtils.Unwrap(response).CompletionReason == "done");
             await client.Files.DeleteFileByIdAsync(fileId: fileToAsk.Id);
         }
@@ -29,8 +29,8 @@ namespace Box.Sdk.Gen.Tests.Integration {
         public async System.Threading.Tasks.Task TestAskAiMultipleItems() {
             FileFull fileToAsk1 = await new CommonsManager().UploadNewFileAsync();
             FileFull fileToAsk2 = await new CommonsManager().UploadNewFileAsync();
-            AiResponseFull response = await client.Ai.CreateAiAskAsync(requestBody: new AiAsk(mode: AiAskModeField.MultipleItemQa, prompt: "Which direction sun rises?", items: Array.AsReadOnly(new [] {new AiItemAsk(id: fileToAsk1.Id, type: AiItemAskTypeField.File) { Content = "Earth goes around the sun" },new AiItemAsk(id: fileToAsk2.Id, type: AiItemAskTypeField.File) { Content = "Sun rises in the East in the morning" }})));
-            Assert.IsTrue(NullableUtils.Unwrap(response).Answer.Contains("East"));
+            AiResponseFull response = await client.Ai.CreateAiAskAsync(requestBody: new AiAsk(mode: AiAskModeField.MultipleItemQa, prompt: "Which direction does the Sun rise?", items: Array.AsReadOnly(new [] {new AiItemAsk(id: fileToAsk1.Id, type: AiItemAskTypeField.File) { Content = "Earth goes around the Sun" },new AiItemAsk(id: fileToAsk2.Id, type: AiItemAskTypeField.File) { Content = "The Sun rises in the east in the morning" }})));
+            Assert.IsTrue(NullableUtils.Unwrap(response).Answer.Contains("east"));
             Assert.IsTrue(NullableUtils.Unwrap(response).CompletionReason == "done");
             await client.Files.DeleteFileByIdAsync(fileId: fileToAsk1.Id);
             await client.Files.DeleteFileByIdAsync(fileId: fileToAsk2.Id);
@@ -39,9 +39,8 @@ namespace Box.Sdk.Gen.Tests.Integration {
         [RetryableTest]
         public async System.Threading.Tasks.Task TestAiTextGenWithDialogueHistory() {
             FileFull fileToAsk = await new CommonsManager().UploadNewFileAsync();
-            AiAgent aiAgentConfig = await client.Ai.GetAiAgentDefaultConfigAsync(queryParams: new GetAiAgentDefaultConfigQueryParams(mode: GetAiAgentDefaultConfigQueryParamsModeField.TextGen) { Language = "en-US" });
-            AiResponse response = await client.Ai.CreateAiTextGenAsync(requestBody: new AiTextGen(prompt: "Parapharse the document.s", items: Array.AsReadOnly(new [] {new AiTextGenItemsField(id: fileToAsk.Id, type: AiTextGenItemsTypeField.File) { Content = "The Earth goes around the sun. Sun rises in the East in the morning." }})) { DialogueHistory = Array.AsReadOnly(new [] {new AiDialogueHistory() { Prompt = "What does the earth go around?", Answer = "The sun", CreatedAt = Utils.DateTimeFromString(dateTime: "2021-01-01T00:00:00Z") },new AiDialogueHistory() { Prompt = "On Earth, where does the sun rise?", Answer = "East", CreatedAt = Utils.DateTimeFromString(dateTime: "2021-01-01T00:00:00Z") }}) });
-            Assert.IsTrue(response.Answer.Contains("sun"));
+            AiResponse response = await client.Ai.CreateAiTextGenAsync(requestBody: new AiTextGen(prompt: "Paraphrase the documents", items: Array.AsReadOnly(new [] {new AiTextGenItemsField(id: fileToAsk.Id, type: AiTextGenItemsTypeField.File) { Content = "The Earth goes around the Sun. The Sun rises in the east in the morning." }})) { DialogueHistory = Array.AsReadOnly(new [] {new AiDialogueHistory() { Prompt = "What does the earth go around?", Answer = "The Sun", CreatedAt = Utils.DateTimeFromString(dateTime: "2021-01-01T00:00:00Z") },new AiDialogueHistory() { Prompt = "On Earth, where does the Sun rise?", Answer = "east", CreatedAt = Utils.DateTimeFromString(dateTime: "2021-01-01T00:00:00Z") }}) });
+            Assert.IsTrue(response.Answer.Contains("Sun"));
             Assert.IsTrue(response.CompletionReason == "done");
             await client.Files.DeleteFileByIdAsync(fileId: fileToAsk.Id);
         }
