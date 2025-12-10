773173de15741d3124e10d462cb9c5fc04bda9b3
box-sdk-build <sdk-build@box.com>
2025-12-10 02:19:48 -0800

    docs: Remove confidence_score and include_confidence_score from AI extract (box/box-openapi#567)

--- Box.Sdk.Gen.NetFramework/Managers/Transfer/TransferManager.cs
+++ Box.Sdk.Gen.NetFramework/Managers/Transfer/TransferManager.cs
@@ -17,7 +17,7 @@ namespace Box.Sdk.Gen.Managers {
         }
         /// <summary>
         /// Move all of the items (files, folders and workflows) owned by a user into
-        /// another user's account
+        /// another user's account.
         /// 
         /// Only the root folder (`0`) can be transferred.
         /// 
