5c3cca937949a52502848d92080aa3d69f891208
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-12-11 11:47:53 +0100

    fix(boxsdkgen): remove duplicate optional tag (box/box-codegen#898) (#1313)

--- BoxSdkGen/Box.Sdk.Gen.NetFramework.Tests.Integration/Test/EnterpriseConfigurations/EnterpriseConfigurationsManagerTests.cs
+++ BoxSdkGen/Box.Sdk.Gen.NetFramework.Tests.Integration/Test/EnterpriseConfigurations/EnterpriseConfigurationsManagerTests.cs
@@ -20,15 +20,15 @@ namespace Box.Sdk.Gen.Tests.Integration {
             string enterpriseId = Utils.GetEnvVar(name: "ENTERPRISE_ID");
             EnterpriseConfigurationV2025R0 enterpriseConfiguration = await adminClient.EnterpriseConfigurations.GetEnterpriseConfigurationByIdV2025R0Async(enterpriseId: enterpriseId, queryParams: new GetEnterpriseConfigurationByIdV2025R0QueryParams(categories: Array.AsReadOnly(new [] {"user_settings","content_and_sharing","security","shield"})));
             Assert.IsTrue(StringUtils.ToStringRepresentation(enterpriseConfiguration.Type?.Value) == "enterprise_configuration");
-            EnterpriseConfigurationUserSettingsV2025R0 userSettings = NullableUtils.Unwrap(NullableUtils.Unwrap(enterpriseConfiguration.UserSettings));
+            EnterpriseConfigurationUserSettingsV2025R0 userSettings = NullableUtils.Unwrap(enterpriseConfiguration.UserSettings);
             Assert.IsTrue(NullableUtils.Unwrap(userSettings.IsEnterpriseSsoRequired).Value == false);
             Assert.IsTrue(NullableUtils.Unwrap(userSettings.NewUserDefaultLanguage).Value == "English (US)");
             Assert.IsTrue(NullableUtils.Unwrap(userSettings.NewUserDefaultStorageLimit).Value == -1);
-            EnterpriseConfigurationContentAndSharingV2025R0 contentAndSharing = NullableUtils.Unwrap(NullableUtils.Unwrap(enterpriseConfiguration.ContentAndSharing));
+            EnterpriseConfigurationContentAndSharingV2025R0 contentAndSharing = NullableUtils.Unwrap(enterpriseConfiguration.ContentAndSharing);
             Assert.IsTrue(NullableUtils.Unwrap(NullableUtils.Unwrap(contentAndSharing.CollaborationPermissions).Value).IsEditorRoleEnabled == true);
-            EnterpriseConfigurationSecurityV2025R0 security = NullableUtils.Unwrap(NullableUtils.Unwrap(enterpriseConfiguration.Security));
+            EnterpriseConfigurationSecurityV2025R0 security = NullableUtils.Unwrap(enterpriseConfiguration.Security);
             Assert.IsTrue(NullableUtils.Unwrap(NullableUtils.Unwrap(security.IsManagedUserSignupEnabled).Value) == false);
-            EnterpriseConfigurationShieldV2025R0 shield = NullableUtils.Unwrap(NullableUtils.Unwrap(enterpriseConfiguration.Shield));
+            EnterpriseConfigurationShieldV2025R0 shield = NullableUtils.Unwrap(enterpriseConfiguration.Shield);
             Assert.IsTrue(NullableUtils.Unwrap(shield.ShieldRules).Count == 0);
         }
 
