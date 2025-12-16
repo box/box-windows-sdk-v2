11b135d7ff8d54e890616faa381dbd8c2686c8a4
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-12-16 15:31:31 +0100

    test(boxsdkgen): fix DocGen test (box/box-codegen#907) (#1331)
    Co-authored-by: box-sdk-build <box-sdk-build@box.com>

--- BoxSdkGen/Box.Sdk.Gen.Tests.Integration.NetStandard/Test/Docgen/DocgenManagerTests.cs
+++ BoxSdkGen/Box.Sdk.Gen.Tests.Integration.NetStandard/Test/Docgen/DocgenManagerTests.cs
@@ -1,8 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Box.Sdk.Gen.Internal;
 using System;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
-using Box.Sdk.Gen.Internal;
 using System.Linq;
 using Box.Sdk.Gen;
 using Box.Sdk.Gen.Schemas;
@@ -18,10 +18,11 @@ namespace Box.Sdk.Gen.Tests.Integration {
         }
         [RetryableTest]
         public async System.Threading.Tasks.Task TestDocgenBatchAndJobs() {
-            FileFull uploadedFile = await new CommonsManager().UploadNewFileAsync();
+            FileFull uploadedFilePdf = await new CommonsManager().UploadNewFileAsync();
+            FileFull uploadedFileDocx = await client.Files.UpdateFileByIdAsync(fileId: uploadedFilePdf.Id, requestBody: new UpdateFileByIdRequestBody() { Name = string.Concat(NullableUtils.Unwrap(uploadedFilePdf.Name), ".docx") });
             FolderFull folder = await new CommonsManager().CreateNewFolderAsync();
-            DocGenTemplateBaseV2025R0 createdDocgenTemplate = await client.DocgenTemplate.CreateDocgenTemplateV2025R0Async(requestBody: new DocGenTemplateCreateRequestV2025R0(file: new FileReferenceV2025R0(id: uploadedFile.Id)));
-            DocGenBatchBaseV2025R0 docgenBatch = await client.Docgen.CreateDocgenBatchV2025R0Async(requestBody: new DocGenBatchCreateRequestV2025R0(file: new FileReferenceV2025R0(id: uploadedFile.Id), inputSource: "api", destinationFolder: new DocGenBatchCreateRequestV2025R0DestinationFolderField(id: folder.Id), outputType: "pdf", documentGenerationData: Array.AsReadOnly(new [] {new DocGenDocumentGenerationDataV2025R0(generatedFileName: "test", userInput: new Dictionary<string, object>() { { "abc", "xyz" } })})));
+            DocGenTemplateBaseV2025R0 createdDocgenTemplate = await client.DocgenTemplate.CreateDocgenTemplateV2025R0Async(requestBody: new DocGenTemplateCreateRequestV2025R0(file: new FileReferenceV2025R0(id: uploadedFileDocx.Id)));
+            DocGenBatchBaseV2025R0 docgenBatch = await client.Docgen.CreateDocgenBatchV2025R0Async(requestBody: new DocGenBatchCreateRequestV2025R0(file: new FileReferenceV2025R0(id: uploadedFileDocx.Id), inputSource: "api", destinationFolder: new DocGenBatchCreateRequestV2025R0DestinationFolderField(id: folder.Id), outputType: "pdf", documentGenerationData: Array.AsReadOnly(new [] {new DocGenDocumentGenerationDataV2025R0(generatedFileName: "test", userInput: new Dictionary<string, object>() { { "abc", "xyz" } })})));
             Assert.IsTrue(docgenBatch.Id != "");
             Assert.IsTrue(StringUtils.ToStringRepresentation(docgenBatch.Type?.Value) == "docgen_batch");
             DocGenJobsV2025R0 docgenBatchJobs = await client.Docgen.GetDocgenBatchJobByIdV2025R0Async(batchId: docgenBatch.Id);
@@ -30,7 +31,7 @@ namespace Box.Sdk.Gen.Tests.Integration {
             Assert.IsTrue(StringUtils.ToStringRepresentation(NullableUtils.Unwrap(docgenBatchJobs.Entries)[0].Type) == "docgen_job");
             Assert.IsTrue(NullableUtils.Unwrap(docgenBatchJobs.Entries)[0].OutputType == "pdf");
             Assert.IsTrue(StringUtils.ToStringRepresentation(NullableUtils.Unwrap(docgenBatchJobs.Entries)[0].Status) != "");
-            Assert.IsTrue(NullableUtils.Unwrap(docgenBatchJobs.Entries)[0].TemplateFile.Id == uploadedFile.Id);
+            Assert.IsTrue(NullableUtils.Unwrap(docgenBatchJobs.Entries)[0].TemplateFile.Id == uploadedFileDocx.Id);
             Assert.IsTrue(NullableUtils.Unwrap(docgenBatchJobs.Entries)[0].Batch.Id == docgenBatch.Id);
             DocGenJobsFullV2025R0 docgenJobs = await client.Docgen.GetDocgenJobsV2025R0Async(queryParams: new GetDocgenJobsV2025R0QueryParams() { Limit = 10000 });
             Assert.IsTrue(NullableUtils.Unwrap(docgenJobs.Entries).Count >= 1);
@@ -59,7 +60,7 @@ namespace Box.Sdk.Gen.Tests.Integration {
             Assert.IsTrue(docgenJob.TemplateFileVersion.Id != "");
             Assert.IsTrue(StringUtils.ToStringRepresentation(docgenJob.Type?.Value) == "docgen_job");
             await client.Folders.DeleteFolderByIdAsync(folderId: folder.Id);
-            await client.Files.DeleteFileByIdAsync(fileId: uploadedFile.Id);
+            await client.Files.DeleteFileByIdAsync(fileId: uploadedFileDocx.Id);
         }
 
     }
