e7d6964d4753753b645d8716d28275861820b4e4
box-sdk-build <sdk-build@box.com>
2026-03-17 05:19:11 -0700

    feat: Add Hub Document API (box/box-openapi#587)

---
+++ Box.Sdk.Gen.Net/Schemas/V2025R0/HubDocumentPagesV2025R0/HubDocumentPagesV2025R0.cs
@@ -0,0 +1,72 @@
+using Box.Sdk.Gen;
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Box.Sdk.Gen.Internal;
+using Box.Sdk.Gen.Schemas;
+
+namespace Box.Sdk.Gen.Schemas {
+    public class HubDocumentPagesV2025R0 : ISerializable {
+        [JsonInclude]
+        [JsonPropertyName("_isnext_markerSet")]
+        protected bool _isNextMarkerSet { get; set; }
+
+        protected string? _nextMarker { get; set; }
+
+        /// <summary>
+        /// Ordered list of pages.
+        /// </summary>
+        [JsonPropertyName("entries")]
+        public IReadOnlyList<HubDocumentPageV2025R0> Entries { get; }
+
+        /// <summary>
+        /// The value will always be `document_pages`.
+        /// </summary>
+        [JsonPropertyName("type")]
+        [JsonConverter(typeof(StringEnumConverter<HubDocumentPagesV2025R0TypeField>))]
+        public StringEnum<HubDocumentPagesV2025R0TypeField> Type { get; }
+
+        /// <summary>
+        /// The limit that was used for these entries. This will be the same as the
+        /// `limit` query parameter unless that value exceeded the maximum value
+        /// allowed. The maximum value varies by API.
+        /// </summary>
+        [JsonPropertyName("limit")]
+        public long? Limit { get; init; }
+
+        /// <summary>
+        /// The marker for the start of the next page of results.
+        /// </summary>
+        [JsonPropertyName("next_marker")]
+        public string? NextMarker { get => _nextMarker; init { _nextMarker = value; _isNextMarkerSet = true; } }
+
+        public HubDocumentPagesV2025R0(IReadOnlyList<HubDocumentPageV2025R0> entries, HubDocumentPagesV2025R0TypeField type = HubDocumentPagesV2025R0TypeField.DocumentPages) {
+            Entries = entries;
+            Type = type;
+        }
+        
+        [JsonConstructorAttribute]
+        internal HubDocumentPagesV2025R0(IReadOnlyList<HubDocumentPageV2025R0> entries, StringEnum<HubDocumentPagesV2025R0TypeField> type) {
+            Entries = entries;
+            Type = HubDocumentPagesV2025R0TypeField.DocumentPages;
+        }
+        internal string? RawJson { get; set; } = default;
+
+        void ISerializable.SetJson(string json) {
+            RawJson = json;
+        }
+
+        string? ISerializable.GetJson() {
+            return RawJson;
+        }
+
+        /// <summary>
+        /// Returns raw json response returned from the API.
+        /// </summary>
+        public Dictionary<string, object?>? GetRawData() {
+            return SimpleJsonSerializer.GetAllFields(this);
+        }
+
+    }
+}
---
+++ Box.Sdk.Gen.Net/Schemas/V2025R0/HubParagraphTextBlockV2025R0/HubParagraphTextBlockV2025R0.cs
@@ -0,0 +1,50 @@
+using Box.Sdk.Gen;
+using System.Text.Json.Serialization;
+using Box.Sdk.Gen.Internal;
+using System.Collections.Generic;
+using Box.Sdk.Gen.Schemas;
+
+namespace Box.Sdk.Gen.Schemas {
+    public class HubParagraphTextBlockV2025R0 : HubDocumentBlockV2025R0, ISerializable {
+        /// <summary>
+        /// The type of this block. The value is always `paragraph`.
+        /// </summary>
+        [JsonPropertyName("type")]
+        [JsonConverter(typeof(StringEnumConverter<HubParagraphTextBlockV2025R0TypeField>))]
+        public StringEnum<HubParagraphTextBlockV2025R0TypeField> Type { get; }
+
+        /// <summary>
+        /// Text content of the block. Includes rich text formatting.
+        /// </summary>
+        [JsonPropertyName("fragment")]
+        public string Fragment { get; }
+
+        public HubParagraphTextBlockV2025R0(string id, string fragment, HubParagraphTextBlockV2025R0TypeField type = HubParagraphTextBlockV2025R0TypeField.Paragraph) : base(id) {
+            Type = type;
+            Fragment = fragment;
+        }
+        
+        [JsonConstructorAttribute]
+        internal HubParagraphTextBlockV2025R0(string id, string fragment, StringEnum<HubParagraphTextBlockV2025R0TypeField> type) : base(id) {
+            Type = HubParagraphTextBlockV2025R0TypeField.Paragraph;
+            Fragment = fragment;
+        }
+        internal new string? RawJson { get; set; } = default;
+
+        void ISerializable.SetJson(string json) {
+            RawJson = json;
+        }
+
+        string? ISerializable.GetJson() {
+            return RawJson;
+        }
+
+        /// <summary>
+        /// Returns raw json response returned from the API.
+        /// </summary>
+        public new Dictionary<string, object?>? GetRawData() {
+            return SimpleJsonSerializer.GetAllFields(this);
+        }
+
+    }
+}
