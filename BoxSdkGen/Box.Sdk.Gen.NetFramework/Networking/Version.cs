1df93e11a3b7b3e8bba582e4296278fc58abefa5
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2026-04-01 16:17:18 +0200

    chore: release version 6.7.0 (#1442)

--- BoxSdkGen/Box.Sdk.Gen.NetFramework/Networking/Version.cs
+++ BoxSdkGen/Box.Sdk.Gen.NetFramework/Networking/Version.cs
@@ -4,6 +4,6 @@ namespace Box.Sdk.Gen.Internal
 {
     static class Version
     {
-        internal static string sdkVersion = "6.6.0";
+        internal static string sdkVersion = "6.7.0";
     }
 }
