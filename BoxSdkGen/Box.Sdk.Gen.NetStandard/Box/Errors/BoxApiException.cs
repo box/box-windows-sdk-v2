96906b2d50b7080ea6acc19f2a38fc1ba24772c4
box-sdk-build <sdk-build@box.com>
2026-04-01 03:44:39 -0700

    fix: Handle empty API error bodies in C# (box/box-codegen#934)

--- BoxSdkGen/Box.Sdk.Gen.NetStandard/Box/Errors/BoxApiException.cs
+++ BoxSdkGen/Box.Sdk.Gen.NetStandard/Box/Errors/BoxApiException.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using Box.Sdk.Gen.Internal;
 
 namespace Box.Sdk.Gen
@@ -11,10 +12,25 @@ namespace Box.Sdk.Gen
 
         public DataSanitizer DataSanitizer { get; set; } = new DataSanitizer();
 
-        public BoxApiException(string message, DateTimeOffset timeStamp, RequestInfo requestInfo, ResponseInfo responseInfo) : base(message, timeStamp, "BoxApiException")
+        public BoxApiException(string message, DateTimeOffset timeStamp, RequestInfo requestInfo, ResponseInfo responseInfo)
+            : base(BuildExceptionMessage(message, responseInfo), timeStamp, "BoxApiException")
         {
             RequestInfo = requestInfo;
             ResponseInfo = responseInfo;
+        }
+
+        private static string BuildExceptionMessage(string message, ResponseInfo responseInfo)
+        {
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                return message;
+            }
+
+            var statusCode = responseInfo?.StatusCode ?? 0;
+            var reasonPhrase = Enum.IsDefined(typeof(HttpStatusCode), statusCode)
+                ? ((HttpStatusCode)statusCode).ToString()
+                : "Unknown";
+            return $"The API returned an unexpected response: [{statusCode} {reasonPhrase}]";
         }
 
         public override string ToString()
