96aa42a646c51fffd33982f5f3685b90d03ac7ff
box-sdk-build <sdk-build@box.com>
2026-04-29 07:53:52 -0700

    SDK-6027: Add new version 2026 (box/box-codegen#945)

---
+++ Box.Sdk.Gen.NetFramework/Schemas/V2026R0/AutomateWorkflowStartRequestV2026R0/AutomateWorkflowStartRequestV2026R0.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Box.Sdk.Gen.Internal;
+
+namespace Box.Sdk.Gen.Schemas {
+    public class AutomateWorkflowStartRequestV2026R0 : ISerializable {
+        /// <summary>
+        /// The callable action ID used to trigger the selected workflow.
+        /// </summary>
+        [JsonPropertyName("workflow_action_id")]
+        public string WorkflowActionId { get; set; }
+
+        /// <summary>
+        /// The files to process with the selected workflow.
+        /// </summary>
+        [JsonPropertyName("file_ids")]
+        public IReadOnlyList<string> FileIds { get; set; }
+
+        public AutomateWorkflowStartRequestV2026R0(string workflowActionId, IReadOnlyList<string> fileIds) {
+            WorkflowActionId = workflowActionId;
+            FileIds = fileIds;
+        }
+        internal string RawJson { get; set; } = default;
+
+        void ISerializable.SetJson(string json) {
+            RawJson = json;
+        }
+
+        string ISerializable.GetJson() {
+            return RawJson;
+        }
+
+        /// <summary>
+        /// Returns raw json response returned from the API.
+        /// </summary>
+        public Dictionary<string, object> GetRawData() {
+            return SimpleJsonSerializer.GetAllFields(this);
+        }
+
+    }
+}
