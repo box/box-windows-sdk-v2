0c68e99fc81b0c40692bc8a74d1f5ffc9ac7d6cb
box-sdk-build <sdk-build@box.com>
2026-04-29 09:00:44 -0700

    feat: Automate Public API (box/box-openapi#594)

---
+++ BoxSdkGen/Box.Sdk.Gen.Net/Managers/AutomateWorkflows/GetAutomateWorkflowsV2026R0Headers.cs
@@ -0,0 +1,25 @@
+using Box.Sdk.Gen;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Box.Sdk.Gen.Internal;
+using Box.Sdk.Gen.Schemas;
+using Box.Sdk.Gen.Parameters;
+
+namespace Box.Sdk.Gen.Managers {
+    public class GetAutomateWorkflowsV2026R0Headers {
+        /// <summary>
+        /// Version header.
+        /// </summary>
+        public StringEnum<BoxVersionHeaderV2026R0> BoxVersion { get; }
+
+        /// <summary>
+        /// Extra headers that will be included in the HTTP request.
+        /// </summary>
+        public Dictionary<string, string?> ExtraHeaders { get; }
+
+        public GetAutomateWorkflowsV2026R0Headers(BoxVersionHeaderV2026R0 boxVersion = BoxVersionHeaderV2026R0._20260, Dictionary<string, string?>? extraHeaders = default) {
+            BoxVersion = boxVersion;
+            ExtraHeaders = extraHeaders ?? new Dictionary<string, string?>() {  };
+        }
+    }
+}
