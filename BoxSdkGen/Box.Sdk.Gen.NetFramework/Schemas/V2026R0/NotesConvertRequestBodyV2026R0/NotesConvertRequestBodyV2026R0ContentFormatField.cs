0b4c19655b2371affd17e65d7837aa3427460524
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2026-05-22 16:51:11 +0200

    feat(boxsdkgen): Add Convert Note API (box/box-codegen#955) (#1535)

---
+++ BoxSdkGen/Box.Sdk.Gen.NetFramework/Schemas/V2026R0/NotesConvertRequestBodyV2026R0/NotesConvertRequestBodyV2026R0ContentFormatField.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel;
+using Box.Sdk.Gen.Schemas;
+using Box.Sdk.Gen.Internal;
+
+namespace Box.Sdk.Gen.Schemas {
+    public enum NotesConvertRequestBodyV2026R0ContentFormatField {
+        [Description("markdown")]
+        Markdown
+    }
+}
---
+++ BoxSdkGen/Box.Sdk.Gen.NetStandard/Managers/Notes/NotesManager.cs
@@ -0,0 +1,37 @@
+using Box.Sdk.Gen;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Box.Sdk.Gen.Internal;
+using Box.Sdk.Gen.Schemas;
+using Box.Sdk.Gen.Parameters;
+
+namespace Box.Sdk.Gen.Managers {
+    public class NotesManager {
+        public IAuthentication Auth { get; set; }
+
+        public NetworkSession NetworkSession { get; set; }
+
+        public NotesManager(NetworkSession networkSession = default) {
+            NetworkSession = networkSession ?? new NetworkSession();
+        }
+        /// <summary>
+        /// Creates a Box Note (`.boxnote` file) from supported source content. See the `content_format` field for supported formats.
+        /// </summary>
+        /// <param name="requestBody">
+        /// Request body of createNoteConvertV2026R0 method
+        /// </param>
+        /// <param name="headers">
+        /// Headers of createNoteConvertV2026R0 method
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Token used for request cancellation.
+        /// </param>
+        public async System.Threading.Tasks.Task<NotesConvertResponseV2026R0> CreateNoteConvertV2026R0Async(NotesConvertRequestBodyV2026R0 requestBody, CreateNoteConvertV2026R0Headers headers = default, System.Threading.CancellationToken cancellationToken = default) {
+            headers = headers ?? new CreateNoteConvertV2026R0Headers();
+            Dictionary<string, string> headersMap = Utils.PrepareParams(map: DictionaryUtils.MergeDictionaries(new Dictionary<string, string>() { { "box-version", StringUtils.ToStringRepresentation(headers.BoxVersion) } }, headers.ExtraHeaders));
+            FetchResponse response = await this.NetworkSession.NetworkClient.FetchAsync(options: new FetchOptions(url: string.Concat(this.NetworkSession.BaseUrls.BaseUrl, "/2.0/notes/convert"), method: "POST", contentType: "application/json", responseFormat: Box.Sdk.Gen.ResponseFormat.Json) { Headers = headersMap, Data = SimpleJsonSerializer.Serialize(requestBody), Auth = this.Auth, NetworkSession = this.NetworkSession, CancellationToken = cancellationToken }).ConfigureAwait(false);
+            return SimpleJsonSerializer.Deserialize<NotesConvertResponseV2026R0>(NullableUtils.Unwrap(response.Data));
+        }
+
+    }
+}
