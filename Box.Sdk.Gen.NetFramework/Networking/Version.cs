b82c735fa2c6eabc5e552fbfdeaecf704b1b25c8
box-sdk-build <sdk-build@box.com>
2026-08-05 03:46:13 -0700

    chore: release version 10.16.0

--- Box.Sdk.Gen.NetFramework/Networking/Version.cs
+++ Box.Sdk.Gen.NetFramework/Networking/Version.cs
@@ -4,6 +4,6 @@ namespace Box.Sdk.Gen.Internal
 {
     static class Version
     {
-        internal static string sdkVersion = "10.15.0";
+        internal static string sdkVersion = "10.16.0";
     }
 }
