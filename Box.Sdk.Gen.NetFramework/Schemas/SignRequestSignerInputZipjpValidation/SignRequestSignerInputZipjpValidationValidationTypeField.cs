ba8a130f3900d75fa44f1d3bd594b4f9fc0e0b7e
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2026-09-03 16:18:30 +0200

    feat: add Japanese ZIP code validation to sign request (box/box-openapi#615) (#1627)

---
+++ Box.Sdk.Gen.NetFramework/Schemas/SignRequestSignerInputZipjpValidation/SignRequestSignerInputZipjpValidationValidationTypeField.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel;
+using Box.Sdk.Gen.Internal;
+
+namespace Box.Sdk.Gen.Schemas {
+    public enum SignRequestSignerInputZipjpValidationValidationTypeField {
+        [Description("zip_jp")]
+        ZipJp
+    }
+}
